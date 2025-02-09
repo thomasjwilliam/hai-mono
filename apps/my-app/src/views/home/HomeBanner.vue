4a4464ee36f2e442b15e05feb5d02f69e607e582
thomasjwilliam <thomas.j.william@gmail.com>
2025-02-09 15:23:28 +0100

    Refactor Home page components and layout structure.
    Split home layout into reusable components: HomeBanner, HomeFeatures.v1, and HomeFeatures.v2. Updated HomeFooter to include additional links and icons. Improved modularity and maintainability of the Home page.

---
+++ apps/my-app/src/views/home/HomeBanner.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import AppButton from "primevue/button";
+</script>
+
+<template>
+  <div class="px-6 py-20 md:px-12 lg:px-20">
+    <div class="text-center">
+      <div class="text-primary font-bold mb-4"><i class="pi pi-users" />&nbsp;POWERED BY YOU</div>
+      <div class="font-bold text-5xl mb-4">Human Accepted Interaction</div>
+      <div class="text-2xl mb-8">A place to write and help your fellow human.</div>
+      <AppButton label="Get Started" raised rounded class="font-bold px-8 py-4 whitespace-nowrap" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+</style>
