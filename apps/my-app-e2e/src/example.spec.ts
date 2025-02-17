fbd6a3cbdf21a52815d32331c50e93f783093790
thomasjwilliam <thomas.j.william@gmail.com>
2025-02-17 13:55:50 +0100

    Update e2e test to validate new UI elements and navigation
    Updated the test to check for updated body text and validate the navigation to the "Dashboard Page" after clicking the "Get Started" button. Adjustments ensure the test reflects the latest UI changes.

--- apps/my-app-e2e/src/example.spec.ts
+++ apps/my-app-e2e/src/example.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 test('has title', async ({ page }) => {
   await page.goto('/');
 
-  // Expect h1 to contain a substring.
-  expect(await page.locator('h1').innerText()).toContain('Welcome');
+  expect(await page.locator('body').innerText()).toContain('POWERED BY YOU');
+  await page.locator('button:has-text("Get Started")').click();
+  expect(await page.locator('h1').innerText()).toBe('Dashboard Page');
 });
