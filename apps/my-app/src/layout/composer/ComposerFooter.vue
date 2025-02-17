ab89a0336ee08a3ab4479a6cc1b7d2dc6f6c80f0
thomasjwilliam <thomas.j.william@gmail.com>
2025-02-17 15:44:24 +0100

    Remove unused components and sample data from the project.
    This commit deletes several unused components, layouts, and corresponding tests to clean up the codebase. Additionally, placeholder/sample data for documents has been removed to streamline the project and eliminate unnecessary files.

--- apps/my-app/src/layout/composer/ComposerFooter.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-
-<template>
-  <div>
-    SAKAI by
-    <a href="https://primevue.org" target="_blank" rel="noopener noreferrer">PrimeVue</a>
-  </div>
-</template>
--- apps/my-app/src/views/composer/ComposerBody.spec.ts
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import ComposerBody from './ComposerBody.vue';
-
-describe('UiVueHome', () => {
-  it('renders properly', () => {
-    const wrapper = mount(ComposerBody, {})
-    expect(wrapper.text()).toContain('Welcome to Hai again!')
-  })
-});
